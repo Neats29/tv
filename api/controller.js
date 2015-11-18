13454a76a3888b5650ebb132defdec95edb113a4
Anita Amini <baadge29@yahoo.co.uk>
2015-11-18 16:18:44 +0000

    successfully get data back from API

--- api/controller.js
+++ api/controller.js
@@ -5,20 +5,23 @@ var controller = {
 	
 	home: {
 		handler: function(request, reply){
+			var shows;
 			
 			wreck.get(uri, function(err, res, payload){
-				if (err) console.log(err);
+				if (err) { throw err; }
 
 				wreck.read(res, null, function (err, body) {
 					if (err) console.log(err);
-					JSON.stringify(payload);
-					console.log("PAYLOAD:", payload);
-					console.log("BODY:",body);
-					return body;
+					else {
+						payload = JSON.parse(payload.toString()); //have to turn to string, otherwise payload only returns the buffer
+	//					console.log("PAYLOAD:", payload.atoz_programmes.elements);
+						shows = payload.atoz_programmes.elements;
+						return body;
+					}
 				});
 			});
 			
-			return reply.view('index');
+			return reply.view('index', { shows: shows });
 		}
 	}
 };
